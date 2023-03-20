e25bf2cac620f0d7e0290ee64a34f5685d903fdd
Pedro Rodolfo Kalva Junior <pedro.kalva@basis.com.br>
2023-03-20 19:14:08 -0300

    adding functionalities to button and text field

--- facedetection/src/App.js
+++ facedetection/src/App.js
@@ -4,19 +4,39 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ input: event.target.value })
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
+  const {input} = this.state;
+    return (
     <div className="App">
       <ParticlesBg className='particles' num={200} type="tadpole" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={this.onInputChange} 
+                     onButtonSubmit={this.onButtonSubmit} />
       { /*
       <FaceRecognition /> */}
     </div>
-  );
+  )};
 }
 
 export default App;
