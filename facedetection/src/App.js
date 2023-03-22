3cda7068ec878fc9487f78f286e0f13c8e8480f0
Pedro Rodolfo Kalva Junior <pedro.kalva@basis.com.br>
2023-03-22 20:50:41 -0300

    Adding blue box on one face

--- facedetection/src/App.js
+++ facedetection/src/App.js
@@ -2,7 +2,6 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Clarifai from 'clarifai';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
 import './App.css';
@@ -25,14 +24,31 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const obj = JSON.parse(data);
+    const clarifaiFace = obj.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box})
   }
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value })
   }
-
-  
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
@@ -46,7 +62,7 @@ class App extends Component {
           {
               "data": {
                   "image": {
-                      "url": IMAGE_URL
+                      "url": this.state.input
                   }
               }
           }
@@ -62,20 +78,11 @@ class App extends Component {
       body: raw
   };
 
-  // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-  // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-  // this will default to the latest version_id
-
   fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
       .then(response => response.text())
-      .then(result => console.log(result))
+      .then(result => this.displayFaceBox(this.calculateFaceLocation(result)))
       .catch(error => console.log('error', error));
   }
-
-  // Run npm install clarifai? Yes
-  // import clarifai at the top; from 'clarifai'
-  // put code onbuttonsubmit function
-
 
   render() {
   const {input} = this.state;
@@ -87,7 +94,7 @@ class App extends Component {
       <Rank />
       <ImageLinkForm onInputChange={this.onInputChange} 
                      onButtonSubmit={this.onButtonSubmit} />
-      <FaceRecognition imageUrl={this.state.imageUrl} />
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
     </div>
   )};
 }
--- facedetection/src/components/FaceRecognition/FaceRecognition.js
+++ facedetection/src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './FaceRecognition.css'
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({ box, imageUrl}) => {
     return (
         <div className="center ma"> 
             <div className='absolute mt2'>
-                <img src={imageUrl} alt="img" width='500px' height='auto'/>
+                <img id='inputImage' src={imageUrl} alt="img" width='500px' height='auto'/>
+                <div className='bouding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
             </div>
         </div>
     );
