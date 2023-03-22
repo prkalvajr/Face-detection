2a51368884f4d9702a0e481e7b66a5abf460ab67
Pedro Rodolfo Kalva Junior <pedro.kalva@basis.com.br>
2023-03-21 21:13:45 -0300

    integration with clarifai API

--- facedetection/src/App.js
+++ facedetection/src/App.js
@@ -1,27 +1,81 @@
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Clarifai from 'clarifai';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
 import './App.css';
 import { Component } from 'react';
+
+// Your PAT (Personal Access Token) can be found in the portal under Authentification
+const PAT = 'a3a9f91a10514e22a3d30c9f36838a17';
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+const USER_ID = 'prkalva';       
+const APP_ID = 'my-first-application';
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '45fb9a671625463fa646c3523a3087d5';    
+const IMAGE_URL = 'https://images.newscientist.com/wp-content/uploads/2022/02/14174128/PRI_223554170.jpg?crop=4:3,smart&width=1200&height=900&upscale=true';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       input: '',
+      imageUrl: '',
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
     this.setState({ input: event.target.value })
   }
 
+  
+
   onButtonSubmit = () => {
-    console.log('click');
+    this.setState({imageUrl: this.state.input});
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+
+  const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+  };
+
+  // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+  // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+  // this will default to the latest version_id
+
+  fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
   }
+
+  // Run npm install clarifai? Yes
+  // import clarifai at the top; from 'clarifai'
+  // put code onbuttonsubmit function
+
 
   render() {
   const {input} = this.state;
@@ -33,8 +87,7 @@ class App extends Component {
       <Rank />
       <ImageLinkForm onInputChange={this.onInputChange} 
                      onButtonSubmit={this.onButtonSubmit} />
-      { /*
-      <FaceRecognition /> */}
+      <FaceRecognition imageUrl={this.state.imageUrl} />
     </div>
   )};
 }
